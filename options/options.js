43865561535f61e6f6ca41cee579f74b901d6712
Crowdbotics <engineering@crowdbotics.com>
2023-05-25 07:33:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-waterfall-de-86070",
+  url: "https://silent-waterfall-de-86070.botics.co",
+  api: "https://silent-waterfall-de-86070.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
